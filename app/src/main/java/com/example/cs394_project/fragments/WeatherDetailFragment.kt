3278f166b8dfd84d31788fa59c3ebc36344c3122
kaan-karahan <kaan.karahan@ozu.edu.tr>
2024-12-01 20:16:33 +0300

    viewmodel and livedata

---
+++ app/src/main/java/com/example/cs394_project/fragments/WeatherDetailFragment.kt
@@ -0,0 +1,33 @@
+package com.example.cs394_project.fragments
+
+import android.annotation.SuppressLint
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.cs394_project.model.District
+import com.example.cs394_project.model.Forecast
+
+class WeatherDetailFragment : ViewModel() {
+/*
+    private val forecast = Forecast()
+
+    private val dailyWeather: MutableLiveData<List<District>> = MutableLiveData()
+    val DailyWeather: LiveData<List<District>> get() = dailyWeather
+
+    private val hourlyWeather: MutableLiveData<List<District>> = MutableLiveData()
+    val HourlyWeather: LiveData<List<District>> get() = hourlyWeather
+
+
+    @SuppressLint("getAPI")
+    fun loadHourlyForecast(zipcode: String) {
+    }
+
+    @SuppressLint("getAPI")
+    fun loadDailyForecast(zipcode: String) {
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+    }
+    */
+}
